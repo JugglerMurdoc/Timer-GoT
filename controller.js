4e61324dff0820cebe57e1aec5baa5511711d1e9
Thibaut EHLINGER <ehlinger.thibaut@gmail.com>
2015-01-08 08:52:40 +0100

    Family object
    Created a "family" object

--- controller.js
+++ controller.js
@@ -1,39 +1,39 @@
 function indexController($scope,$http,$interval){
 	
-    initFamillies();
+    initFamilies();
 
     var startAllButtonText='START ALL'
     var startAllButtonIs
 
     /**
      * []
-     * @param  {[type]} famillyName [description]
+     * @param  {[type]} familyName [description]
      * @return {[type]}           [description]
      */
-     $scope.toggleTimer = function(famillyName) {  
-                //var famillyTimersArray = getTimersAjax(famillyName)
-                var matchingFamilly = getFamillyObject(famillyName);
-                if(matchingFamilly.timerIsRunning){
-                    matchingFamilly.timerIsRunning = false;
-                    $interval.cancel(matchingFamilly.timer);
-                    matchingFamilly.timer = undefined;
+     $scope.toggleTimer = function(familyName) {  
+                //var familyTimersArray = getTimersAjax(familyName)
+                var matchingfamily = getfamilyObject(familyName);
+                if(matchingfamily.timerIsRunning){
+                    matchingfamily.timerIsRunning = false;
+                    $interval.cancel(matchingfamily.timer);
+                    matchingfamily.timer = undefined;
                 }
                 else{
-                 matchingFamilly.timerIsRunning = true;   
-                 matchingFamilly.timer =  $interval(function(){
-                    matchingFamilly.remainingTime -= 1000;
+                 matchingfamily.timerIsRunning = true;   
+                 matchingfamily.timer =  $interval(function(){
+                    matchingfamily.remainingTime -= 1000;
                 },1000);
              }
 
-             $scope.toggleButtonText(matchingFamilly);
+             $scope.toggleButtonText(matchingfamily);
                 
          }
 
     $scope.startAllButtonClick = function(){
-        for(var fam in $scope.famillies){
-            if($scope.famillies[fam].timerIsRunning === undefined || $scope.famillies[fam].timerIsRunning === false){
+        for(var fam in $scope.families){
+            if($scope.families[fam].timerIsRunning === undefined || $scope.families[fam].timerIsRunning === false){
                 
-                $scope.toggleTimer($scope.famillies[fam].name);
+                $scope.toggleTimer($scope.families[fam].name);
             }
         }
     }   ;
@@ -41,12 +41,12 @@ function indexController($scope,$http,$interval){
 
     /**
      * [Does the ajax call to get the timer objects.]
-     * @param  {[string or string array]} famillyName [Contains the name of the familly, with its first character capitalized.]
+     * @param  {[string or string array]} familyName [Contains the name of the family, with its first character capitalized.]
      * @return {[An array of objects]} data [The timers object]
      */
-     $scope.getTimersAjax = function(familly){
+     $scope.getTimersAjax = function(family){
         //AJAX Call
-        var responsePromise = $http.get("http://192.168.1.12:4242/timers?FAMILIES[]="+familly.name);
+        var responsePromise = $http.get("http://192.168.1.12:4242/timers?FAMILIES[]="+family.name);
 
         responsePromise.success(function(data, status, headers, config) {
             return data;
@@ -59,56 +59,65 @@ function indexController($scope,$http,$interval){
 
     /**
      * [Changes the text of the timer button after a click]
-     * @param  {[type]} famillyName [The name of the familly who's buttons has been pressed.]
+     * @param  {[type]} familyName [The name of the family who's buttons has been pressed.]
      */
-     $scope.toggleButtonText = function(familly){
-         familly.buttonText = (familly.timerIsRunning)?'S T O P':'S T A R T';
+     $scope.toggleButtonText = function(family){
+         family.buttonText = (family.timerIsRunning)?'S T O P':'S T A R T';
 
      }
 
     /**
-     * [returns the familly object matching the given famillyName]
-     * @param  {[String]} famillyName [The name of the familly we're looking for]
-     * @return {[Object]} familly [The matching familly object]
+     * [returns the family object matching the given familyName]
+     * @param  {[String]} familyName [The name of the family we're looking for]
+     * @return {[Object]} family [The matching family object]
      */
 
-     function getFamillyObject(famillyName)
+     function getfamilyObject(familyName)
      {
-        var n = $scope.famillies.length;
+        var n = $scope.families.length;
         var i;
         for(i = 0; i < n; i++){
-            var currentName = $scope.famillies[i].name; 
-            if(currentName === famillyName){
-                return $scope.famillies[i];
+            var currentName = $scope.families[i].name; 
+            if(currentName === familyName){
+                return $scope.families[i];
             }
         } 
 
-        throw "Familly name has no match in $scope" 
+        throw "family name has no match in $scope" 
     }
+
+    var family = function(familyName){
+        this.name = familyName;
+        this.buttonText =  'START';
+        this.timerIsRunning = false;
+        this.picture  = "./Ressources/family_pictures/"+familyName+".jpg";
+        this.remainingTime = new Date(0,0,0,3,0,0,0);
+    };
+
 
     /**
      * [Initiate de families informations]
      * @return {[type]} [description]
      */
-     function initFamillies(){
+     function initfamilies(){
         /**
-     * [Basic information about each familly]
+     * [Basic information about each family]
      * @type {Array}
      */
 
-     $scope.famillies = [
-     {name : 'Greyjoy', buttonText : 'S T A R T', timerIsRunning : false, picture : "./Ressources/Familly_pictures/Greyjoy.jpg", remainingTime : undefined},
-     {name : 'Baratheon', buttonText : 'S T A R T' , timerIsRunning : false, picture : "./Ressources/Familly_pictures/Baratheon.jpg", remainingTime : undefined},
-     {name : 'Lannister', buttonText : 'S T A R T', timerIsRunning : false, picture : "./Ressources/Familly_pictures/Lannister.jpg", remainingTime : undefined},
-     {name : 'Stark', buttonText : 'S T A R T', timerIsRunning :  false, picture : "./Ressources/Familly_pictures/Stark.jpg", remainingTime : undefined},
-     {name : 'Tyrell', buttonText : 'S T A R T', timerIsRunning : false, picture : "./Ressources/Familly_pictures/Tyrell.jpg", remainingTime : undefined},
-     {name : 'Martell', buttonText : 'S T A R T',timerIsRunning : false, picture : "./Ressources/Familly_pictures/Martell.jpg", remainingTime : undefined}
+     $scope.families = [
+     {name : 'Greyjoy', buttonText : 'S T A R T', timerIsRunning : false, picture : "./Ressources/family_pictures/Greyjoy.jpg", remainingTime : undefined},
+     {name : 'Baratheon', buttonText : 'S T A R T' , timerIsRunning : false, picture : "./Ressources/family_pictures/Baratheon.jpg", remainingTime : undefined},
+     {name : 'Lannister', buttonText : 'S T A R T', timerIsRunning : false, picture : "./Ressources/family_pictures/Lannister.jpg", remainingTime : undefined},
+     {name : 'Stark', buttonText : 'S T A R T', timerIsRunning :  false, picture : "./Ressources/family_pictures/Stark.jpg", remainingTime : undefined},
+     {name : 'Tyrell', buttonText : 'S T A R T', timerIsRunning : false, picture : "./Ressources/family_pictures/Tyrell.jpg", remainingTime : undefined},
+     {name : 'Martell', buttonText : 'S T A R T',timerIsRunning : false, picture : "./Ressources/family_pictures/Martell.jpg", remainingTime : undefined}
      ]
 
-     var n = $scope.famillies.length;
+     var n = $scope.families.length;
      var i;
      for(i = 0; i < n; i++){
-        $scope.famillies[i].remainingTime = new Date(0,0,0,3,0,0,0);
+        $scope.families[i].remainingTime = new Date(0,0,0,3,0,0,0);
     }
 }
 
