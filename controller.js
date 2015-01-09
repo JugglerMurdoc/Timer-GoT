90389404a9f880f8e6b1bccfe27fd95feaaaaa01
EHLINGER Thibaut <ehlinger.thibaut@gmail.com>
2015-01-09 09:24:52 +0100

    POO
    Familys are now objects with methods. StartTimer(), stopTimer() are not
    global functions any more.

--- controller.js
+++ controller.js
@@ -1,115 +1,113 @@
-function indexController($scope,$http,$interval){
-	
-    initFamillies();
+function indexController($scope, $http, $interval) {
 
-    var startAllButtonText='START ALL'
-    var startAllButtonIs
-
-    /**
-     * []
-     * @param  {[type]} famillyName [description]
-     * @return {[type]}           [description]
-     */
-     $scope.toggleTimer = function(famillyName) {  
-                //var famillyTimersArray = getTimersAjax(famillyName)
-                var matchingFamilly = getFamillyObject(famillyName);
-                if(matchingFamilly.timerIsRunning){
-                    matchingFamilly.timerIsRunning = false;
-                    $interval.cancel(matchingFamilly.timer);
-                    matchingFamilly.timer = undefined;
-                }
-                else{
-                 matchingFamilly.timerIsRunning = true;   
-                 matchingFamilly.timer =  $interval(function(){
-                    matchingFamilly.remainingTime -= 1000;
-                },1000);
-             }
-
-             $scope.toggleButtonText(matchingFamilly);
-                
-         }
-
-    $scope.startAllButtonClick = function(){
-        for(var fam in $scope.famillies){
-            if($scope.famillies[fam].timerIsRunning === undefined || $scope.famillies[fam].timerIsRunning === false){
-                
-                $scope.toggleTimer($scope.famillies[fam].name);
-            }
-        }
-    }   ;
-
-
-    /**
-     * [Does the ajax call to get the timer objects.]
-     * @param  {[string or string array]} famillyName [Contains the name of the familly, with its first character capitalized.]
-     * @return {[An array of objects]} data [The timers object]
-     */
-     $scope.getTimersAjax = function(familly){
-        //AJAX Call
-        var responsePromise = $http.get("http://192.168.1.12:4242/timers?FAMILIES[]="+familly.name);
-
-        responsePromise.success(function(data, status, headers, config) {
-            return data;
-        });
-        responsePromise.error(function(data, status, headers, config) {
-         console.log(data); 
-
-     });
-    }
-
-    /**
-     * [Changes the text of the timer button after a click]
-     * @param  {[type]} famillyName [The name of the familly who's buttons has been pressed.]
-     */
-     $scope.toggleButtonText = function(familly){
-         familly.buttonText = (familly.timerIsRunning)?'S T O P':'S T A R T';
-
-     }
-
-    /**
-     * [returns the familly object matching the given famillyName]
-     * @param  {[String]} famillyName [The name of the familly we're looking for]
-     * @return {[Object]} familly [The matching familly object]
-     */
-
-     function getFamillyObject(famillyName)
-     {
-        var n = $scope.famillies.length;
-        var i;
-        for(i = 0; i < n; i++){
-            var currentName = $scope.famillies[i].name; 
-            if(currentName === famillyName){
-                return $scope.famillies[i];
-            }
-        } 
-
-        throw "Familly name has no match in $scope" 
-    }
 
     /**
      * [Initiate de families informations]
      * @return {[type]} [description]
      */
-     function initFamillies(){
-        /**
-     * [Basic information about each familly]
-     * @type {Array}
+    initFamilies();
+
+   
+    $scope.startAllButtonClick = function () {
+        for (var i in $scope.families) {
+            $scope.families[i].startTimer();
+        }
+    }
+
+
+    $scope.getTimersAjax = function (family) {
+        //AJAX Call
+        var responsePromise = $http.get("http://192.168.1.12:4242/timers?FAMILIES[]=" + family.name);
+
+        responsePromise.success(function (data, status, headers, config) {
+            return data;
+        });
+        responsePromise.error(function (data, status, headers, config) {
+            console.log(data);
+
+        });
+    }
+
+    /**
+     * [returns the family object matching the given familyName]
+     * @param  {[String]} familyName [The name of the family we're looking for]
+     * @return {[Object]} family [The matching family object]
      */
 
-     $scope.famillies = [
-     {name : 'Greyjoy', buttonText : 'S T A R T', timerIsRunning : false, picture : "./Ressources/Familly_pictures/Greyjoy.jpg", remainingTime : undefined},
-     {name : 'Baratheon', buttonText : 'S T A R T' , timerIsRunning : false, picture : "./Ressources/Familly_pictures/Baratheon.jpg", remainingTime : undefined},
-     {name : 'Lannister', buttonText : 'S T A R T', timerIsRunning : false, picture : "./Ressources/Familly_pictures/Lannister.jpg", remainingTime : undefined},
-     {name : 'Stark', buttonText : 'S T A R T', timerIsRunning :  false, picture : "./Ressources/Familly_pictures/Stark.jpg", remainingTime : undefined},
-     {name : 'Tyrell', buttonText : 'S T A R T', timerIsRunning : false, picture : "./Ressources/Familly_pictures/Tyrell.jpg", remainingTime : undefined},
-     {name : 'Martell', buttonText : 'S T A R T',timerIsRunning : false, picture : "./Ressources/Familly_pictures/Martell.jpg", remainingTime : undefined}
-     ]
+    function getFamillyObject(familyName) {
+        var n = $scope.families.length;
+        var i;
+        for (i = 0; i < n; i++) {
+            var currentName = $scope.families[i].name;
+            if (currentName === familyName) {
+                return $scope.families[i];
+            }
+        }
 
-     var n = $scope.famillies.length;
-     var i;
-     for(i = 0; i < n; i++){
-        $scope.famillies[i].remainingTime = new Date(0,0,0,3,0,0,0);
+        throw "Familly name has no match in $scope"
     }
-}
+
+    function Family(familyName) {
+        var timerThread = undefined;
+        this.name = familyName;
+        this.buttonText = 'S T A R T';
+        this.timerIsRunning = false;
+        this.picture = "./Ressources/Familly_pictures/" + familyName + ".jpg";
+        this.remainingTime = new Date(0, 0, 0, 3, 0, 0, 0);
+        this.buttonTimerButtonClick = function () {
+            this.toggleTimer();
+        }
+        this.toggleTimer = function () {
+            if (this.timerIsRunning) {
+                this.stopTimer();
+            }
+            else {
+                this.startTimer();
+            }
+        }
+        this.startTimer = function () {
+            var that = this;
+            if (!(this.timerIsRunning)) {
+                that.timerIsRunning = true;
+                that.timerThread = $interval(function () { $scope.updateTime(that) }, 1000);
+            }
+
+            this.toggleButtonText();
+        }
+        this.stopTimer = function () {
+            if (this.timerIsRunning) {
+                $interval.cancel(this.timerThread);
+                this.timerThread = undefined;
+                this.timerIsRunning = false;
+
+                this.toggleButtonText();
+            }
+        }
+        this.toggleButtonText = function () {
+            this.buttonText = (this.timerIsRunning) ? 'S T O P' : 'S T A R T';
+        }
+        
+
+    }
+
+    $scope.updateTime = function(family) {
+        console.log(family);
+        family.remainingTime -= 1000;
+
+    }
+
+    function initFamilies() {
+        /**
+     * [Basic information about each family]
+     * @type {Array}
+     */
+        $scope.families = new Array();
+        $scope.families.push(new Family('Greyjoy'));
+        $scope.families.push(new Family('Baratheon'));
+        $scope.families.push(new Family('Lannister'));
+        $scope.families.push(new Family('Stark'));
+        $scope.families.push(new Family('Tyrell'));
+        $scope.families.push(new Family('Martell'));
+    }
 
 }
